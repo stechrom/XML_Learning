18689505f4f3ce74d257d6cf9bad5a0ade62a685
Roman Stechkevych <roman.stechkevych@tietoevry.com>
2021-07-09 12:37:07 +0300

    [Intellij, main]: The first commit of project files.

---
+++ src/xml/learning/LearningXML.java
@@ -0,0 +1,12 @@
+package xml.learning;
+
+/*
+    This class is created to learn operations with XML/XSD files.
+    Author: Roman Stechkevych
+ */
+public class LearningXML {
+
+    public static void main(String[] args) {
+        System.out.println("Let's start learning XML and XSD");
+    }
+}
